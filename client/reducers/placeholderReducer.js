db0f3633684c5f629bafb7c28ca6135620ab4c9d
Eric Gomez <ergomez0201@gmail.com>
2022-03-15 15:25:30 -0700

    setting up boilerplate for redux reducer
    Co-authored-by: Bryan Bart <MrBeeAreWhy@users.noreply.github.com>

--- client/reducers/placeholderReducer.js
+++ client/reducers/placeholderReducer.js
@@ -1,18 +1,60 @@
 import * as types from '../constants/actionTypes';
 
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+  this.prev = null;
+}
+
 const initialState = {
-  placeholderState: null,
+  activeUser: null,
+  jobs: [],
+  interested: new LinkedList(),
+  applied: new LinkedList(),
+  interviewOne: new LinkedList(),
+  interviewTwo: new LinkedList(),
+  offered: new LinkedList(),
 };
 
+/*
 
+  cards stored on each stateful key like so:
+ { id: {title: string, company: string, etc...}, ... }
+
+*/
 
 const placeholderReducer = (state = initialState, action) => {
   switch (action.type) {
-    case types.ADD_CARD:
-      let newPlaceholder = true;
+    case types.POPULATE_COLUMNS:
+      // call a function that parses thru the response from the server
+      // expect to get [ {job data from database} ]
+      // iterate through the server response that contains jobs
+      // for each index in that response array
+      //  check the object's 'status' key
+      //  add that object to the appropriate state object based on the status key
+      // return the update state
       return {
         ...state,
-        placeholderState: newPlaceholder,
+      };
+
+    case types.DELETE_CARD:
+      // delete action.payload.<columnName>.<id>
+      return {
+        ...state,
+      };
+
+    case types.MOVE_CARD:
+      // delete from previous column
+
+      // add to new column
+
+      return {
+        ...state,
       };
 
     default: {
@@ -22,3 +64,9 @@ const placeholderReducer = (state = initialState, action) => {
 };
 
 export default placeholderReducer;
+
+/*
+
+need an action that updates the activeUser
+
+*/
