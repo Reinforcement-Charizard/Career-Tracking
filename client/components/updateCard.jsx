5714296c3e29c293736b3f96004e4b34d7077932
David Lee <davidlee@Davids-MacBook-Pro-2.local>
2022-03-16 11:54:28 -0700

    updates to components, adding basic routing methods

--- client/components/updateCard.jsx
+++ client/components/updateCard.jsx
@@ -10,36 +10,55 @@ const mapDispatchToProps = dispatch => ({
 }); 
 
 
+
+
 const updateCard = (props) => {
 //recreate card but everything's an input field
-console.log('inside updateCard')
+// console.log('inside updateCard')
 
-const updateCardEvent = (props) => {
+const updateCardEvent = () => {
   // build structure of request
+  const reqBody = {
+    user_id: props.user_id,
+    title: document.getElementById('title').innerText,
+    company: document.getElementById('company').innerText,
+    date: document.getElementByClassName('date').innerText,
+    notes: document.getElementByClassName(notesArea).innerText,
+    contact_email: document.getElementById('email').innerText,
+    contact_phone: document.getElementById('number').innerText,
+    url: document.getElementById('url').innerText,
+    status: document.getElementsByClassName('selectClass').innerText,
+  }
 
   // await response
-  fetch('https://localhost:8080/api/updateJob')
+  fetch('https://localhost:8080/api/updateJob', {
+    method: 'POST',
+    body: reqBody,
+    headers: { 'Content-Type': 'application/json'}
+  })
     .then((res) => {res.json();})
-    .then((res) => {this.props.updateCardAction(res.locals)})
+    .then((res) => {this.props.updateCardAction(res)})
     .catch((err) => {console.log('error in updateCardEvent: ', err)})
 };
 
 
 return (
 <div className="updateCard">
-  <div> UPDATE CARD </div> 
-  <label>Title: </label>
+  <div className="updateCardDiv"> UPDATE CARD </div> 
+  <p></p>
+  <label className="updateTitle">Title: </label>
     <span>
-      <input placeholder= 'SWE2' type="text"/>  
+      <input id='title' placeholder= 'SWE2' type="text"/>  
     </span>
-  <label> Company: </label>
+  <p></p>
+  <label className='updateCompany'> Company: </label>
     <span> 
-      <input placeholder= 'Google' type="text"/>
+      <input id='company' placeholder= 'Google' type="text"/>
     </span> 
     <p>
       <label className = "currentDate"> Application Date: </label>
         <span>
-            <input placeholder={props.date} type="text"/>
+            <input id='date' placeholder={props.date} type="text"/>
         </span>
     </p>      
     <p>
@@ -52,31 +71,51 @@ return (
         <p>
           <label className='contactEmail'> Email: </label>
           <span> 
-            <input placeholder={props.contactEmail} />
+            <input id='email' placeholder={props.contactEmail} />
           </span>
+        </p>
+        <p>
           <label className='contactNumber'> Number: </label>
           <span> 
-              <input placeholder={props.contactNumber} />
+              <input id='number' placeholder={props.contactNumber} />
           </span>
         </p>
     <p>
       <label className = "companyURL"> URL: </label>
       <span> 
-          <input placeholder={props.companyURL} />
+          <input id='url' placeholder={props.companyURL} />
       </span>
     </p>
     <p>
       <label className = "status"> Status: </label>
       <span> 
-          <input placeholder={props.status} />
+          <select className="selectClass">
+              <option value="interested">Interested</option>
+              <option value="applied">Applied</option>
+              <option value="interviewOne">Interview #1</option>
+              <option value="interviewTwo">Interview #2</option>
+              <option value="offered">Offered</option>
+            </select>
       </span>
     </p>
+    <div className="notesDiv">
+            <p className="notesLabel">Notes</p>
+            <textarea  className="notesArea" rows="5" cols="36" placeholder='Notes'/>
+          </div>
     <div className="updateCardButtons">
       <button className="updateCardSubmit" onClick = {(e) => {
         e.preventDefault();
-        updateCardEvent(props);
+        updateCardEvent();
+        document.getElementById('title').innerText = ''
+        document.getElementById('company').innerText = ''
+        document.getElementByClassName('date').innerText = ''
+        document.getElementByClassName(notesArea).innerText = ''
+        document.getElementById('email').innerText = ''
+        document.getElementById('number').innerText = ''
+        document.getElementById('url').innerText = ''
+        document.getElementsByClassName('selectClass').innerText = ''
       }}>Submit</button>
-      <button onClick= {(e)=>{window.location='http://localhost:8080/home'}}>Cancel</button>
+      <button onClick= {(e)=>{window.location.assign('http://localhost:8080/home')}}>Cancel</button>
     </div>
 </div>
 );
---
+++ client/containers/CardsContainer.jsx
@@ -0,0 +1,15 @@
+import CardsDisplay from '../components/CardsDisplay.jsx'
+
+
+const CardsContainer = () => {
+
+
+return (
+    <div id='container'>
+        <CardsDisplay />
+    </div>
+)
+
+}
+
+export default CardsContainer;
