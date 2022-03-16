b13cc3e09962c636738f87260acba438191495d5
Eric Gomez <ergomez0201@gmail.com>
2022-03-16 11:52:18 -0700

    created functions to traverse linkedlist and pass in props to card component. Populate columns with card data

--- client/components/CardCreator.jsx
+++ client/components/CardCreator.jsx
@@ -1,28 +1,23 @@
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form';
 import { connect } from 'react-redux';
 import * as actions from '../actions/actions';
-import './componentStyling/cardCreator.scss'
+import './componentStyling/cardCreator.scss';
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   // create card
   updateCardAction: (createObject) => {
-    dispatch(actions.updateCardAction(createObject))
-  }
+    dispatch(actions.updateCardAction(createObject));
+  },
+});
 
-
-})
-
-
-const CardCreator = () => {
+function CardCreator() {
   // /api/addJob
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => {
-    
-  }
+  const onSubmit = (data) => {};
 
   const date = new Date().toLocaleDateString().substring(0, 10); // -> 3/15/22
-  const [todaysDate, setTodaysDate] = useState(new Date().toISOString().substring(0, 10))
+  const [todaysDate, setTodaysDate] = useState(new Date().toISOString().substring(0, 10));
 
   return (
     <div className="cardCreator">
@@ -30,21 +25,39 @@ const CardCreator = () => {
       <div className="formDiv">
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="inputDiv">
-            <input className="inputClass" placeholder='Job Title' {...register("title")}/>
-            <input className="inputClass" placeholder='Company' {...register("company")}/>
+            <input className="inputClass" placeholder="Job Title" {...register('title')} />
+            <input className="inputClass" placeholder="Company" {...register('company')} />
             <div className="dateDiv">
               <label>Date Applied</label>
-                <input className="dateInput" type="date" defaultValue={todaysDate} {...register("dateApplied")}/>
+              <input
+                className="dateInput"
+                type="date"
+                defaultValue={todaysDate}
+                {...register('dateApplied')}
+              />
             </div>
             <div className="dateDiv">
               <label>Interview Date</label>
               <span>
-                <input className="dateInput" type="date" placeholder='Interview Date' {...register("intDate")}/>
+                <input
+                  className="dateInput"
+                  type="date"
+                  placeholder="Interview Date"
+                  {...register('intDate')}
+                />
               </span>
             </div>
-            <input className="inputClass" placeholder='Contact Email' {...register("contactEmail")}/>
-            <input className="inputClass" placeholder='Contact Phone' {...register("contactPhone")}/>
-            <input className="inputClass" placeholder='URL' {...register("url")}/>
+            <input
+              className="inputClass"
+              placeholder="Contact Email"
+              {...register('contactEmail')}
+            />
+            <input
+              className="inputClass"
+              placeholder="Contact Phone"
+              {...register('contactPhone')}
+            />
+            <input className="inputClass" placeholder="URL" {...register('url')} />
             <select className="selectClass">
               <option value="interested">Interested</option>
               <option value="applied">Applied</option>
@@ -55,17 +68,21 @@ const CardCreator = () => {
           </div>
           <div className="notesDiv">
             <p id="notesLabel">Notes</p>
-            <textarea  className="notesArea" rows="5" cols="36" placeholder='Notes' {...register("notes")}/>
+            <textarea
+              className="notesArea"
+              rows="5"
+              cols="36"
+              placeholder="Notes"
+              {...register('notes')}
+            />
           </div>
         </form>
       </div>
-      <div className='addJobButton'>
-        <button>
-          Add Job  
-        </button>
+      <div className="addJobButton">
+        <button>Add Job</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default connect(null, mapDispatchToProps)(CardCreator);
--- client/reducers/placeholderReducer.js
+++ client/reducers/placeholderReducer.js
@@ -11,10 +11,10 @@ function ListNode(val) {
   this.prev = null;
 }
 
-//adds node to the end of the linkedList
-LinkedList.prototype.add = function(val) {
+// adds node to the end of the linkedList
+LinkedList.prototype.add = function (val) {
   const newNode = new ListNode(val);
-  //check if this node is the first node in linkedlist
+  // check if this node is the first node in linkedlist
   if (this.head === null) {
     this.head = newNode;
     this.tail = newNode;
@@ -23,13 +23,13 @@ LinkedList.prototype.add = function(val) {
   this.tail.next = newNode;
   newNode.prev = this.tail;
   this.tail = newNode;
-}
+};
 
-//removes first instance of the passed in value from linkedList
-LinkedList.prototype.remove = function(val) {
-  //set current to this.head
+// removes first instance of the passed in value from linkedList
+LinkedList.prototype.remove = function (val) {
+  // set current to this.head
   let currNode = this.head;
-  //check if val is the head
+  // check if val is the head
   if (this.head.val.hasOwnProperty(val)) {
     this.head = currNode.next;
     this.head.prev = null;
@@ -61,80 +61,6 @@ const initialState = {
   offered: new LinkedList(),
 };
 
-//dummy data
-const testData = [
-  {
-    title: 'Software Engineer',
-    companyName: 'Google',
-    interviewDate: Date.now(),
-    status: 'applied',
-    cardId: '1',
-  },
-  {
-    title: 'Software Dev',
-    companyName: 'Meta',
-    interviewDate: Date.now(),
-    status: 'applied',
-    cardId: '2',
-  },
-  {
-    title: 'Software Eng',
-    companyName: 'Netflix',
-    interviewDate: Date.now(),
-    status: 'interviewOne',
-    cardId: '3',
-  },
-  {
-    title: 'Data Analyst',
-    companyName: 'Target',
-    interviewDate: Date.now(),
-    status: 'interviewTwo',
-    cardId: '4',
-  },
-  {
-    title: 'Software Developer',
-    companyName: 'Codesmith',
-    interviewDate: Date.now(),
-    status: 'offered',
-    cardId: '5',
-  },
-  {
-    title: 'UX Designer',
-    companyName: 'Amazon',
-    interviewDate: Date.now(),
-    status: 'applied',
-    cardId: '6',
-  },
-  {
-    title: 'UI Designer',
-    companyName: 'Best Buy',
-    interviewDate: Date.now(),
-    status: 'offered',
-    cardId: '7',
-  },
-  {
-    title: 'Software Engineer',
-    companyName: 'Samsung',
-    interviewDate: Date.now(),
-    status: 'applied',
-    cardId: '8',
-  },
-  {
-    title: 'Software Developer',
-    companyName: 'Hulu',
-    interviewDate: Date.now(),
-    status: 'interviewTwo',
-    cardId: '9',
-  },
-  {
-    title: 'Software Engineer',
-    companyName: 'HBO',
-    interviewDate: Date.now(),
-    status: 'interviewOne',
-    cardId: '10',
-  },
-]
-
 /*
 
   cards stored on each stateful key like so:
@@ -145,24 +71,25 @@ const testData = [
 const placeholderReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.POPULATE_COLUMNS:
-      const updatedState = {...state};
-      for (let i = 0; i < action.payload.length; i++){
+      console.log('this is the action payload in populate columns: ', action.payload);
+      const updatedState = { ...state };
+      for (let i = 0; i < action.payload.length; i++) {
         const addedCard = {};
         addedCard[action.payload[i]._id] = action.payload[i];
         updatedState[action.payload[i].status].add(addedCard);
       }
-
+      console.log('this is the updatedState: ', updatedState);
       return {
-        updatedState,
+        ...updatedState,
       };
 
     case types.DELETE_CARD:
-      const column = {...state[action.payload[0].status]};
+      const column = { ...state[action.payload[0].status] };
       column.remove(action.payload[0]._id);
 
       return {
         ...state,
-        column
+        column,
       };
     case types.ACTIVE_USER:
       return {
